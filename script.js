eedf476a6644c0d62b2ca2184d61268c22a29e11
Bryce St. Pierre <brycelstpierre@gmail.com>
2021-12-04 13:04:31 -0500

    Updated semantic HTML.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Script');
+
+
+  // Show section/tooltip on click.
+
+  // Hide section/tooltip on click again and also focus lost.
 });
