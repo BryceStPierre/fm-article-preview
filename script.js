9451fc14746e6ec12e945ddbd95adf0455cffa67
Bryce St. Pierre <brycelstpierre@gmail.com>
2021-12-07 00:02:12 -0500

    Completed desktop version.

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+const toggleShare = () => {
+  document
+    .querySelector(".card__footer")
+    .classList.toggle("card__footer--share");
+  document.querySelector(".share").classList.toggle("share--open");
+  document.querySelector("#shareBtn").classList.toggle("btn--alt");
+};
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#shareBtn").addEventListener("click", toggleShare);
 
-  document.querySelector('#shareBtn').addEventListener('click', (e) => {
-    document.querySelector('.card__footer').classList.toggle('card__footer--share');
-    document.querySelector('#shareBtn').classList.toggle('btn--alt');
+  document.querySelector("#shareBtn").addEventListener("blur", (e) => {
+    var nextTarget = e.relatedTarget;
+    var actions = document.querySelectorAll(".share__actions > a");
+    var nextTargetIsAction = false;
+    actions.forEach(function (action) {
+      nextTargetIsAction ||= nextTarget === action;
+    });
+
+    if (nextTargetIsAction) return;
+
+    toggleShare();
   });
-
-  // Show section/tooltip on click.
-
-  // Hide section/tooltip on click again and also focus lost.
 });
